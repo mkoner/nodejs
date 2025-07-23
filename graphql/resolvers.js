e2c0878b3750580e0f34012d832f080bf7f5e385
mkoner <amzid@hotmail.fr>
2025-07-23 18:55:12 -0500

    Handle associations with resolvers

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -8,5 +8,31 @@ export const resolvers = {
         game: (_, { id }) => games.find(game => game.id === id),
         review: (_, { id }) => reviews.find(review => review.id === id),
         author: (_, { id }) => authors.find(author => author.id === id),
-    }
+    },
+    Game: {
+        reviews: (game) => reviews.filter(review => review.game_id === game.id),
+    },
+    Author: {
+        reviews: (author) => reviews.filter(review => review.author_id === author.id),
+    },
+    Review: {
+        // game(review) {
+        //     return games.find(game => game.id === review.game_id);
+        // },
+        // author(review) {
+        //     return authors.find(author => author.id === review.author_id);
+        // },
+        game: (review) => games.find(game => game.id === review.game_id),
+        author: (review) => authors.find(author => author.id === review.author_id),
+    },
 }
+
+/*
+In Apollo Server, a GraphQL resolver function typically receives four arguments,
+in the following order:
+(parent, args, context, info) => { ... }
+1. parent
+The result returned from the resolver of the parent field.
+Useful when resolving nested fields.
+
+*/
--- graphql/schema.js
+++ graphql/schema.js
@@ -3,15 +3,19 @@ export const typeDefs = `#graphql
     id: ID!,
     title: String!,
     platform: [String!]!,
+    reviews: [Review!],
     }
     type Review {
     id: ID!,
     rating: Int!,
     comment: String,
+    game: Game!,
+    author: Author!,
     }
     type Author {
     id: ID!,
     name: String!, 
+    reviews: [Review!],
     }
     type Query {
     games: [Game],
